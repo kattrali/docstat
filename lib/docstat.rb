79480583bb5a8b6100ab76bbca81fb596c895667
Delisa Mason <iskanamagus@gmail.com>
2014-03-29 16:56:31 -0700

    Move logging out of library, document processing

--- lib/docstat.rb
+++ lib/docstat.rb
@@ -6,20 +6,53 @@ require 'docstat/token'
 
 module DocStat
 
+  # Parses a docset and returns a representation of the
+  # documented tokens within the library.
+  # The general structure is as follows:
+  #
+  # {
+  #   'ratio': decimal
+  #   'containers': [
+  #     {
+  #       'name': 'class name',
+  #       'tokens': [
+  #         {
+  #           'name': 'name of token',
+  #           'type': 'property or message type',
+  #           'abstract': 'description of token',
+  #           'declaration': 'formal declaration',
+  #           'returns': 'description of return value',
+  #           'documented': boolean
+  #         }, ...
+  #       ]
+  #     }, ...
+  #   ]
+  # }
   def self.process docset_path
-    db_path    = File.join(docset_path, 'Contents/Resources/docSet.dsidx')
-    containers = Container.from_sqlite(db_path)
-    tokens     = containers.map(&:tokens).flatten
+    containers = containers_from_docset(docset_path)
+    { "containers" => containers.map(&:to_hash) , "ratio" => overall_ratio(containers) }
+  end
+
+  def self.test_ratio docset_path, passing_ratio
+    containers = containers_from_docset(docset_path)
+    ratio = overall_ratio(containers)
+    ratio >= passing_ratio
+  end
+
+  private
+
+  def self.containers_from_docset docset_path
+    db_path = File.join(docset_path, 'Contents/Resources/docSet.dsidx')
+    Container.from_sqlite(db_path)
+  end
+
+  def self.overall_ratio containers
+    tokens = containers.map(&:tokens).flatten
     if tokens.size > 0
       overall_ratio = tokens.select(&:documented?).size.to_f/tokens.size
     else
       overall_ratio = 1
     end
-    print "#{tokens.size} tokens, #{((overall_ratio*1000).to_i/1000.0) * 100}% documented"
-    { "containers" => containers.map(&:to_hash) , "ratio" => overall_ratio }
-  end
-
-  def self.test_ratio docset_path, passing_ratio
-    false
+    overall_ratio
   end
 end
