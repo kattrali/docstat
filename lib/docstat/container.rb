aa457be8f9aa6a2d122d320b1476722a6fb71351
Delisa Mason <iskanamagus@gmail.com>
2014-04-01 16:14:09 -0700

    Fix docstat breaking on libraries without return values

--- lib/docstat/container.rb
+++ lib/docstat/container.rb
@@ -6,16 +6,24 @@ module DocStat
   class Container
     attr_reader :name, :tokens
 
-    QUERY = "select ZCONTAINERNAME,ZTOKENNAME,ZTYPENAME,ZTOKENMETAINFORMATION.ZABSTRACT,ZDECLARATION,ZRETURNVALUE.ZABSTRACT from ZTOKEN \
+    QUERY = "select ZCONTAINERNAME,ZTOKENNAME,ZTYPENAME,ZTOKENMETAINFORMATION.ZABSTRACT,ZDECLARATION from ZTOKEN \
+      left join ZCONTAINER on ZTOKEN.ZCONTAINER = ZCONTAINER.Z_PK \
+      left join ZTOKENTYPE on ZTOKEN.ZTOKENTYPE = ZTOKENTYPE.Z_PK \
+      left join ZTOKENMETAINFORMATION on ZTOKEN.Z_PK = ZTOKENMETAINFORMATION.ZTOKEN \
+      order by ZCONTAINERNAME"
+
+    QUERY_WITH_RETURN_VALUES = "select ZCONTAINERNAME,ZTOKENNAME,ZTYPENAME,ZTOKENMETAINFORMATION.ZABSTRACT,ZDECLARATION,ZRETURNVALUE.ZABSTRACT from ZTOKEN \
       left join ZCONTAINER on ZTOKEN.ZCONTAINER = ZCONTAINER.Z_PK \
       left join ZTOKENTYPE on ZTOKEN.ZTOKENTYPE = ZTOKENTYPE.Z_PK \
       left join ZTOKENMETAINFORMATION on ZTOKEN.Z_PK = ZTOKENMETAINFORMATION.ZTOKEN \
       left join ZRETURNVALUE on ZTOKENMETAINFORMATION.ZRETURNVALUE = ZRETURNVALUE.Z_PK \
       order by ZCONTAINERNAME"
 
+    RETURNVALUES_QUERY = "SELECT * FROM sqlite_master WHERE type='table'"
+
     def self.from_sqlite path
       db = SQLite3::Database.new(path)
-      rows = db.execute(QUERY).to_a
+      rows = has_return_values?(db) ? db.execute(QUERY_WITH_RETURN_VALUES) : db.execute(QUERY)
       container_groups = rows.group_by {|r| r.first }
       groups = container_groups.map do |name, tokens|
         self.new(name, tokens)
@@ -26,6 +34,10 @@ module DocStat
 
     def initialize name, tokens
       @name, @tokens = name, tokens.map {|t| Token.new(t)}
+    end
+
+    def self.has_return_values? database
+      database.execute(RETURNVALUES_QUERY).detect {|r| r[1] == 'ZRETURNVALUE'} != nil
     end
 
     def to_hash
